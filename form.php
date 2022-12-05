b8a180d8c56e531b433ec414b0e386294a239527
slavik <76749567+fireoricecream@users.noreply.github.com>
2022-12-05 11:01:27 +0200

    Create form.php

---
+++ form.php
@@ -0,0 +1,13 @@
+<?php
+
+$username = 'Dmitry';
+$password = 'Dmitry';
+?>
+
+<form name="registration">
+    <label for="username">username</label>
+    <input type="text" name="username" value="<?php echo $username ?>">
+    <label for="password">password</label>
+    <input type="password" name="password" value="<?php echo $password ?>">
+    <button form="registration">sign up</button>
+</form>
