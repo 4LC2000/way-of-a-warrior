cf46015792629dcb8639876ec927e8186b2a2b25
slavik <76749567+fireoricecream@users.noreply.github.com>
2022-12-12 15:22:46 +0200

    Update form.php

--- form.php
+++ form.php
@@ -2,8 +2,12 @@
 
 $language = ["ua", "ru", "en", "fr", "de"];
 $username = "1";
-$password = "Dmitry";
+$password = "dgdfgfdgdfg";
 $lang = ["ru" => "Привет!", "en" => "Hello!", "ua" => "Привіт!", "fr" => "Hé!", "de" => "Hallo!"];
+$date = "31-12-2020";
+$london = "london is the capital of great britain";
+$randomStr = "1a2b3c4b5d6e7f8g9h0";
+$text = "Главным фактором языка РНР является практичность. РНР должен предоставить программисту средства для быстрого и эффективного решения поставленных задач. Практический характер РНР обусловлен пятью важными характеристиками: традиционностью, простотой, эффективностью, безопасностью, гибкостью. Существует еще одна «характеристика», которая делает РНР особенно привлекательным: он распространяется бесплатно! Причем, с открытыми исходными кодами ( Open Source ). Язык РНР будет казаться знакомым программистам, работающим в разных областях. Многие конструкции языка позаимствованы из Си, Perl. Код РНР очень похож на тот, который встречается в типичных программах на С или Pascal. Это заметно снижает начальные усилия при изучении РНР. PHP — язык, сочетающий достоинства Perl и Си и специально нацеленный на работу в Интернете, язык с универсальным (правда, за некоторыми оговорками) и ясным синтаксисом. И хотя PHP является довольно молодым языком, он обрел такую популярность среди web-программистов, что на данный момент является чуть ли не самым популярным языком для создания web-приложений (скриптов).";
 $users = [];
 
 $users["0"] = ["name" => "Ivan", "email" => "test@test.com", "lang" => "en"];
@@ -110,6 +114,48 @@ function reverseUsers($users)
 {
     return array_reverse($users);
 }
+// - в переменной $date лежит дата формата '31-12-2020'. Преобразуйте эту дату в формат '2020.12.31'.
+function convertData($date)
+{
+    return str_replace("-", ".", $date);
+}
+// - дана строка 'london is the capital of great britain'. Сделайте из нее строку 'London Is The Capital Of Great Britain'
+function convertFirstCharToUpperCase($london)
+{
+    return ucwords($london);
+}
+// дана переменная $password, в которой хранится пароль пользователя. Если количество символов пароля больше 7 и меньше 12, то выведите пользователю сообщение о том, что пароль подходит, иначе: сообщение о том, что нужно придумать другой пароль.
+function validationPass($password)
+{
+    return strlen($password) > 7 && strlen($password) < 12 ? print("Отличный пароль!") : print("Плохой пароль!");
+}
+// - дана строка с буквами и цифрами, например, '1a2b3c4b5d6e7f8g9h0'. Удалите из нее все цифры. В нашем случае должна получится строка 'abcbdefgh'
+function deleteInt($randomStr)
+{
+    return print(preg_replace("/[0-9]/", '', $randomStr));
+}
+
+function validateText($text)
+{
+
+    $limiter = 80;
+    $strings = explode("<br>", wordwrap($text, $limiter, "<br>"));
+
+
+    foreach ($strings as $key => $string) {
+        $space = $limiter - strlen($string);
+        $words = str_word_count($string, 1, "АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя:-,.«»—");
+        foreach ($words as $key => $word) {
+            if (!($key == array_key_last($words))) {
+                $words[$key] = $word .  " ";
+                implode(" ", $words);
+            }
+        }
+        echo "<pre>";
+        var_dump($words);
+    }
+}
+
 ?>
 
 <form id="registration">
